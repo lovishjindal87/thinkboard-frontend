c3bfcd1939af8665b7e60b2a36d21a0bd28d5daf
Lovish Jindal <117459981+lovishjindal87@users.noreply.github.com>
2025-12-04 20:20:12 +0530

    landing page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -103,8 +103,8 @@ const HomePage = () => {
 
       <div className="max-w-7xl mx-auto p-4 mt-6">
         {loading && <div className="text-center text-primary py-10">Loading notes...</div>}
-
-        <div className="mb-6 flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
+        {notes.length > 0 &&
+          <div className="mb-6 flex flex-col gap-3 md:flex-row md:items-center md:justify-between">
           <div className="form-control w-full md:max-w-sm">
             <input
               type="text"
@@ -129,16 +129,17 @@ const HomePage = () => {
               </select>
             </div>
           </div>
-        </div>
-
+          </div>
+        }
         {!authLoading && !user && (
-          <div className="max-w-2xl mx-auto mt-6 text-center">
-            <h2 className="text-2xl font-semibold mb-2 text-base-content">
+          <div className="max-w-5xl mx-auto mt-6 text-center">
+            <h2 className="text-4xl font-semibold mb-2 text-base-content">
               Welcome to Thinkboard Tasks
             </h2>
-            <p className="text-base-content/70">
+            <p className="text-base-content/70 text-3xl">
               Please sign in with Google to manage your tasks.
             </p>
+            <img className=" w-full h-full mt-24" src="/sample-homepage.png" />
           </div>
         )}
 
